b4c7e97d60f71a0f3827468636f29eb230917b3f
roib20 <66280613+roib20@users.noreply.github.com>
2022-11-24 02:51:24 +0200

    Add Birds, Rabbits, Reptiles and Rodents to animal json

--- flask_app/static/base.js
+++ flask_app/static/base.js
@@ -1,16 +1,16 @@
-import animalObject from './animals.js';
+import animalsObject from '/static/animals.js';
 
 window.onload = function () {
     let animalSel = document.getElementById("animal");
     let breedSel = document.getElementById("breed");
-    for (let x in animalObject) {
+    for (let x in animalsObject) {
         animalSel.options[animalSel.options.length] = new Option(x, x);
     }
     animalSel.onchange = function () {
         //empty Breeds dropdown
         breedSel.length = 1;
         //display correct values
-        for (let y in animalObject[this.value]) {
+        for (let y in animalsObject[this.value]) {
             breedSel.options[breedSel.options.length] = new Option(y, y);
         }
     }
