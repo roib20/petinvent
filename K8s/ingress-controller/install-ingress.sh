a2726df2dfec0fe85f1d9f2ed5265e21f5bbc5d7
roib20 <66280613+roib20@users.noreply.github.com>
2022-11-24 13:15:05 +0200

    Update install-ingress.sh shell script

--- K8s/ingress-controller/install-ingress.sh
+++ K8s/ingress-controller/install-ingress.sh
@@ -1,34 +1,32 @@
 #! /bin/sh
 
-helm repo add nginx-stable https://helm.nginx.com/stable
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
-helm uninstall nginx-ingress--namespace nginx-ingress
+helm uninstall ingress-nginx --namespace ingress-nginx
 helm uninstall cert-manager --namespace default
 helm uninstall external-dns --namespace default
 
-helm upgrade --install nginx-ingress nginx-stable/nginx-ingress \
---namespace nginx-ingress --create-namespace \
---set controller.enableCustomResources=true \
---set controller.enableCertManager=true \
---set controller.enableExternalDNS=true \
---set controller.setAsDefaultIngress=true
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+--namespace ingress-nginx --create-namespace \
+--set controller.ingressClassResource.default=true \
+--set controller.metrics.enabled=true
 
-kubectl get services --namespace nginx-ingress
+kubectl get services --namespace ingress-nginx
 
 helm upgrade --install cert-manager jetstack/cert-manager \
 --version v1.10.0 \
 --namespace default \
 --set installCRDs=true
 
-kubectl get services --namespace nginx-ingress
+kubectl get services --namespace ingress-nginx
 
 /bin/sh ./secret.sh
 
 kubectl apply -f clusterissuer-staging.yaml
-kubectl apply -f certificate.yaml
+kubectl apply -f certificate-staging.yaml
 
 helm install external-dns bitnami/external-dns \
 --version 6.11.3 \
